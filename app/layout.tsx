7622350d56590d21011aa91231f432f01bf39f14
DavidJones <qowera@gmail.com>
2025-09-27 01:05:55 +0800

    feat: add speed-insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
 import { Nav } from './Nav'
@@ -20,7 +21,7 @@ export const metadata: Metadata = {
   description: 'Two Factor Authentication',
 }
 
-interface RootLayoutProps {
+export interface RootLayoutProps {
   children: React.ReactNode
 }
 
@@ -30,6 +31,7 @@ export default function RootLayout(props: Readonly<RootLayoutProps>) {
   return (
     <html lang="en">
       <Analytics />
+      <SpeedInsights />
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen flex flex-col`}>
         <Nav />
         {children}
